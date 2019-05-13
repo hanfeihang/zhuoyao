c0956e2b00e1087d0a4eaacc58c3738f92f8e850
踏古 <feihang.hfh@alibaba-inc.com>
2019-05-13 13:42:51 +0800

    readme

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var gpx = require('./routes/gpx');
+var gps = require('./routes/iOS_gps');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/gpx', gpx);
+app.use('/gps', gps);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
